3558c8132db85f0fbe66a78f02e3395197492851
joosterws <65594584+joosterws@users.noreply.github.com>
2023-09-13 15:29:07 +0200

    Dropdown functional

--- Reuse.js
+++ Reuse.js
@@ -3,24 +3,23 @@ class NavbarReuse extends HTMLElement{
         this.innerHTML = `
         <ul id="primary-navigation" data-visible="false" class="primary-navigation flex">
             <li>
-                <a href="/index.html">
-                    Home
-                </a>
+                <a href="/index.html">Home</a>
             </li>
             <li>
-                <a href="/HU_projects.html">
-                    HU projects
-                </a>
+                <a href="/HU_projects.html">HU projects</a>
             </li>
             <li>
-                <a href="/my_history.html">
-                    My history
-                </a>
+                <a href="/my_history.html">My history</a>
             </li>
             <li>
-                <a href="/SelfHosted/SelfHosted.html">
-                    SelfHosted
-                </a>
+                <div class="dropdown">
+                    <a href="/SelfHosted/SelfHosted.html">Self Hosted</a>
+                        <div class="dropdown-content">
+                            <a href="/SelfHosted/unRaid.html">Unraid</a>
+                            <a href="/SelfHosted/NextCloud.html">Nextcloud</a>
+                            <a href="/SelfHosted/HomeAssistant.html">HomeAssistant</a>
+                        </div>
+                </div>
             </li>
         </ul>`
     }
