b7099595243c1c112b8ae909e6980888972f63e6
joosterws <65594584+joosterws@users.noreply.github.com>
2023-09-18 10:07:56 +0200

    Foto gallery

--- Reuse.js
+++ Reuse.js
@@ -21,7 +21,8 @@ class NavbarReuse extends HTMLElement{
                         </div>
                 </div>
             </li>
-        </ul>`
+        </ul>
+        `
     }
 }
 customElements.define('app-navbar', NavbarReuse);
@@ -36,7 +37,8 @@ class SidebarReuse extends HTMLElement{
                 <li><a href="https://www.facebook.com"><img src="/images/Social-SVG/facebook.svg" alt="Facebook icon"></a></li>
                 <li><a href="https://www.twitter.com"><img src="/images/Social-SVG/twitter.svg"alt="Twitter icon"></a></li>
                 <li><a href="https://www.instagram.com"><img src="/images/Social-SVG/instagram.svg"alt="Instagram icon"></a></li>
-            </ul>`
+            </ul>
+            `
     }
 }
 customElements.define('app-sidebar', SidebarReuse);
@@ -51,7 +53,8 @@ class HeaderButtonsReuse extends HTMLElement{
 
         <button id="mobile-nav-toggle" class="mobile-nav-toggle">
             <svg width="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"/></svg>
-        </button>`
+        </button>
+        `
     }
 }
 customElements.define('app-header-buttons', HeaderButtonsReuse);
@@ -60,14 +63,15 @@ class FooterReuse extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
         <div class="social-links">
-            <a href="https://www.facebook.com"><img src="/images/Social-SVG/facebook.svg" alt="Facebook icon" style="width:24px;height:24px"></a>
-            <a href="https://www.twitter.com"><img src="/images/Social-SVG/twitter.svg" alt="Twitter icon" style="width:24px;height:24px"></a>
-            <a href="https://www.instagram.com"><img src="/images/Social-SVG/instagram.svg" alt="Instagram icon" style="width:24px;height:24px"></a>
-            <a href="https://www.linkedin.com/"><img src="/images/Social-SVG/linkedin.svg" alt="Linkedin icon" style="width:24px;height:24px"></a>
+            <a href="https://www.facebook.com" target="_blank"><img src="/images/Social-SVG/facebook.svg" alt="Facebook icon" style="width:24px;height:24px"></a>
+            <a href="https://www.twitter.com" target="_blank"><img src="/images/Social-SVG/twitter.svg" alt="Twitter icon" style="width:24px;height:24px"></a>
+            <a href="https://www.instagram.com" target="_blank"><img src="/images/Social-SVG/instagram.svg" alt="Instagram icon" style="width:24px;height:24px"></a>
+            <a href="https://www.linkedin.com/in/joost-sluijmer/" target="_blank"><img src="/images/Social-SVG/linkedin.svg" alt="Linkedin icon" style="width:24px;height:24px"></a>
         </div>
-        <a href="cookies-policy.html">
+        <!-- <a href="cookies-policy.html">
             Cookies policy
-        </a>`
+        </a> -->
+        `
     }
 }
 customElements.define('app-footer', FooterReuse);
