09a9219370bf491b711173c2ba284f4a70cd87a8
BuildTools <unconfigured@null.spigotmc.org>
2023-09-22 12:37:58 +0200

    Social links updated with titles

--- scripts/Reuse.js
+++ scripts/Reuse.js
@@ -64,10 +64,10 @@ class FooterReuse extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
         <div class="social-links">
-            <a href="https://www.facebook.com" target="_blank"><img src="/images/Social-SVG/facebook.svg" alt="Facebook icon" style="width:24px;height:24px"></a>
-            <a href="https://www.twitter.com" target="_blank"><img src="/images/Social-SVG/twitter.svg" alt="Twitter icon" style="width:24px;height:24px"></a>
-            <a href="https://www.instagram.com" target="_blank"><img src="/images/Social-SVG/instagram.svg" alt="Instagram icon" style="width:24px;height:24px"></a>
-            <a href="https://www.linkedin.com/in/joost-sluijmer/" target="_blank"><img src="/images/Social-SVG/linkedin.svg" alt="Linkedin icon" style="width:24px;height:24px"></a>
+            <a href="https://www.linkedin.com/in/joost-sluijmer/" target="_blank"><img src="/images/Social-SVG/linkedin.svg" title="Linkedin" alt="Linkedin icon" style="width:24px;height:24px"></a>
+            <a href="https://github.com/joosterws" target="_blank"><img src="/images/Social-SVG/github.svg"  title="Github" alt="Github icon" title="Linkedin" style="width:24px;height:24px"></a>
+            <a href="" target="_blank"><img src="/images/Social-SVG/at.svg" title="Email" alt="Email contact" style="width:24px;height:24px"></a>
+
         </div>
         <!-- <a href="cookies-policy.html">
             Cookies policy
