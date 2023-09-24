49c322feef85d7927088d8d7940fa659d0cdd6a2
joosterws <65594584+joosterws@users.noreply.github.com>
2023-09-24 21:09:01 +0200

    header button tiles

--- scripts/Reuse.js
+++ scripts/Reuse.js
@@ -48,11 +48,11 @@ class HeaderButtonsReuse extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
         <button id="dark-mode-toggle" class="dark-mode-toggle">
-            <img src="/images/Sun.svg" alt="Dark Mode" style="width:30px;height:30px">
+            <img src="/images/Sun.svg" title="Dark mode" alt="Dark Mode" style="width:30px;height:30px">
         </button>
 
         <button id="mobile-nav-toggle" class="mobile-nav-toggle">
-            <img src="/images/Burger-menu.svg" alt="Menu" style="width:30px;height:30px">
+            <img src="/images/Burger-menu.svg" title="Menu" alt="Menu" style="width:30px;height:30px">
         </button>
         `
     }
