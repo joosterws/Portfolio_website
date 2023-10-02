36dac45bf646e15b98cf7a53446d81dfa720c26f
joosterws <65594584+joosterws@users.noreply.github.com>
2023-10-02 11:06:58 +0200

    fold mobiile nav when clicked next

--- scripts/Reuse.js
+++ scripts/Reuse.js
@@ -2,27 +2,27 @@ class NavbarReuse extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
         <nav>
-        <ul id="primary-navigation" data-visible="false" class="primary-navigation flex">
-            <li>
-                <a href="/index.html">Home</a>
-            </li>
-            <li>
-                <a href="/HU_projects.html">HU projects</a>
-            </li>
-            <li>
-                <a href="/my_history.html">My history</a>
-            </li>
-            <li>
-                <div class="dropdown">
-                    <a href="/SelfHosted/SelfHosted.html">Self Hosted</a>
-                    <div class="dropdown-content">
-                        <a href="/SelfHosted/unRaid.html">Unraid</a>
-                        <a href="/SelfHosted/NextCloud.html">Nextcloud</a>
-                        <a href="/SelfHosted/HomeAssistant.html">Home Assistant</a>
+            <ul id="primary-navigation" nav-visible="false" class="primary-navigation flex">
+                <li>
+                    <a href="/index.html">Home</a>
+                </li>
+                <li>
+                    <a href="/HU_projects.html">HU projects</a>
+                </li>
+                <li>
+                    <a href="/my_history.html">My history</a>
+                </li>
+                <li>
+                    <div class="dropdown">
+                        <a href="/SelfHosted/SelfHosted.html">Self Hosted</a>
+                        <div class="dropdown-content">
+                            <a href="/SelfHosted/unRaid.html">Unraid</a>
+                            <a href="/SelfHosted/NextCloud.html">Nextcloud</a>
+                            <a href="/SelfHosted/HomeAssistant.html">Home Assistant</a>
+                        </div>
                     </div>
-                </div>
-            </li>
-        </ul>
+                </li>
+            </ul>
         </nav>
         `
     }
--- scripts/mobile-nav-button.js
+++ scripts/mobile-nav-button.js
@@ -1,12 +1,28 @@
 const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
-navToggle.addEventListener('click', () => {
-    const visibility = primaryNav.getAttribute('data-visible')
-    // console.log(visibility)
-    if (visibility === "false") {
-        primaryNav.setAttribute("data-visible", true) 
-    } else if (visibility === "true") {
-        primaryNav.setAttribute("data-visible", false) 
+// Make primaryNav focusable
+primaryNav.setAttribute('tabindex', '-1');
+
+// Handle blur once, outside the click event
+primaryNav.addEventListener('blur', (event) => {
+    // Check if the newly focused element is inside primaryNav
+    if (primaryNav.contains(event.relatedTarget)) {
+        return;
     }
-})
+
+    // Use a timeout to allow other events to be processed first
+    setTimeout(() => {
+        primaryNav.setAttribute("nav-visible", "false");
+    }, 0);
+});
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('nav-visible');
+    if (visibility === "false") {
+        primaryNav.setAttribute("nav-visible", "true"); 
+        primaryNav.focus();
+    } else if (visibility === "true") {
+        primaryNav.setAttribute("nav-visible", "false");
+    }
+});
