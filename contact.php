9619ab4b20460884dde5464b4ce1e478868a34ee
joosterws <65594584+joosterws@users.noreply.github.com>
2023-10-12 09:45:54 +0200

    DB with php working

--- contact.php
+++ contact.php
@@ -1,42 +1,42 @@
 <?php
-// // Database connection settings
-// $host = 'localhost';
-// $username = 'root';
-// $password = '';
-// $dbname = 'db_contact';
+// Database connection settings
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'db_contact';
 
-// // Create a database connection
-// $conn = mysqli_connect($host, $username, $password, $dbname);
+// Create a database connection
+$conn = mysqli_connect($host, $username, $password, $dbname);
 
-// // Check connection
-// if (!$conn) {
-//     // die("Connection failed: " . mysqli_connect_error());
-//     echo "test0";
-// }
-// echo "test1";
-// // Process form data
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $first_name = $_POST["contactFirstname"];
-//     $last_name = $_POST["contactLastName"];
-//     $email = $_POST["contactEmail"];
-//     $message = $_POST["contactMessage"];
-//     echo "test2";
+// Check connection
+if (!$conn) {
+    // die("Connection failed: " . mysqli_connect_error());
+    echo "test0";
+}
+echo "test1";
+// Process form data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $first_name = $_POST["contactFirstname"];
+    $last_name = $_POST["contactLastName"];
+    $email = $_POST["contactEmail"];
+    $message = $_POST["contactMessage"];
+    echo "test2";
 
 
 
-//     // Insert data into the database
-//     $sql = "INSERT INTO contact-form (first_name, last_name, email, message) VALUES ('$first_name', '$last_name', '$email', '$message')";
+    // Insert data into the database
+    $sql = "INSERT INTO contactform (first_name, last_name, email, message) VALUES ('$first_name', '$last_name', '$email', '$message')";
 
-//     if (mysqli_query($conn, $sql)) {
-//         echo "Form submitted successfully!";
-//     } else {
-//         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-//     }
-//     echo "test3";
+    if (mysqli_query($conn, $sql)) {
+        echo "Form submitted successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    echo "test3";
 
-//     // Close the database connection
-//     mysqli_close($conn);
-// }
+    // Close the database connection
+    mysqli_close($conn);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
